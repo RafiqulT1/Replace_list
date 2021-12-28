088080a14cfddafe191c5384b5a709ff76c9b313
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2021-12-28 13:26:50 +0200

    add check_input() function

--- replace_list.py
+++ replace_list.py
@@ -2,6 +2,17 @@ def display_list():
     print("Here is current list: ", value_list)
 value_list = [1, 2, 3, 4, 5]
 
+def check_input():
+    while True:
+        try:
+            index = int(input("Type number between 1-5 to select position for replacement: "))
+        except ValueError:
+            print("Sorry worng value! :(")
+        else:
+            return index
+
+
 display_list()
+print(check_input())
 
 #testing
